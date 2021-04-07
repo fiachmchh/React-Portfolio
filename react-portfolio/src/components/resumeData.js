c2018fe92342d5dd481c8a03ea83b8b203b62910
fiachmchh <fiachmchh@gmail.com>
2021-04-06 23:48:55 -0400

    react portfolio skeleton and file structure

---
+++ react-portfolio/src/components/resumeData.js
@@ -0,0 +1,89 @@
+let resumeData = {
+    "imagebaseurl":"https://ivy555.github.io/",
+    "name": "Fiach",
+    "role": "Full Stack Web Dev Student",
+    "linkedinId":"Your ID",
+    "roleDescription": "Role description here",
+    "socialLinks":[
+    {
+    "name":"linkedin",
+    "url":"https://www.linkedin.com/in/fiachmchh/",
+    "className":"fa fa-linkedin"
+    },
+    {
+    "name":"github",
+    "url":"https://github.com/fiachmchh",
+    "className":"fa fa-github"
+    }
+    ],
+    "aboutme":"About me here",
+    "address":"Astoria, NYC",
+    "website":"",
+    "education":[
+    {
+    "UniversityName":"University of Liverpool",
+    "specialization":"Modern History and Politics",
+    "MonthOfPassing":"Aug",
+    "YearOfPassing":"2013",
+    "Achievements":"Add achievements here"
+    },
+    {
+    "UniversityName":"Columbia Engineering",
+    "specialization":"Full Stack Flex Web Development",
+    "MonthOfPassing":"Apr",
+    "YearOfPassing":"2021",
+    "Achievements":"Achievements here"
+    }
+    ],
+    "work":[
+    {
+    "CompanyName":"PMac Hospitality",
+    "specialization":"job",
+    "MonthOfLeaving":"Jan",
+    "YearOfLeaving":"2018",
+    "Achievements":"roles"
+    },
+    {
+    "CompanyName":"PMac Hospitality",
+    "specialization":"job",
+    "MonthOfLeaving":"Jan",
+    "YearOfLeaving":"2018",
+    "Achievements":"roles"
+    }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+    {
+    "skillname":"HTML5"
+    },
+    {
+    "skillname":"CSS"
+    },
+    {
+    "skillname":"Reactjs"
+    }
+    ],
+    "portfolio":[
+    {
+    "name":"project1",
+    "description":"app name",
+    "imgurl":""
+    },
+    {
+    "name":"project2",
+    "description":"app name",
+    "imgurl":".jpg"
+    },
+    {
+    "name":"project3",
+    "description":"app name",
+    "imgurl":".png"
+    },
+    {
+    "name":"project4",
+    "description":"mobileapp",
+    "imgurl":"images/portfolio/phone.jpg"
+    }
+    ],
+    }
+    export default resumeData
